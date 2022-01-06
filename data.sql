62aba8cab8689f413c6287a43a339e01249daced
Jabez <93544976+Jakunot@users.noreply.github.com>
2022-01-06 11:18:03 +0100

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,22 @@
+-- Inserting data
+
+-- person
+insert into person (personal_id, first_name, last_name, city, state, postal_code, email_address, phone_nr) values ('540-18-6260', 'Léonie', 'Mathison', 'Åkersberga', 'Stockholm', '184 34', 'fmathison0@columbia.edu', '4921143094');
+insert into person (personal_id, first_name, last_name, city, state, postal_code, email_address, phone_nr) values ('601-36-5031', 'Léana', 'Inns', 'Stockholm', 'Stockholm', '101 22', 'minns1@sciencedaily.com', '3821120041');
+insert into person (personal_id, first_name, last_name, city, state, postal_code, email_address, phone_nr) values ('483-32-8341', 'Adélie', 'Sliman', 'Vallentuna', 'Stockholm', '186 53', 'asliman2@tamu.edu', '3923173633');
+insert into person (personal_id, first_name, last_name, city, state, postal_code, email_address, phone_nr) values ('375-20-0736', 'Estée', 'McCrory', 'Täby', 'Stockholm', '183 60', 'smccrory3@uol.com.br', '6599912341');
+insert into person (personal_id, first_name, last_name, city, state, postal_code, email_address, phone_nr) values ('577-74-9657', 'Marie-hélène', 'Treadwell', 'Stockholm', 'Stockholm', '106 65', 'gtreadwell4@toplist.cz', '8669917227');
+insert into person (personal_id, first_name, last_name, city, state, postal_code, email_address, phone_nr) values ('283-78-6693', 'Bécassine', 'Malter', 'Lidingö', 'Stockholm', '181 85', 'bmalter5@gravatar.com', '2572440164');
+insert into person (personal_id, first_name, last_name, city, state, postal_code, email_address, phone_nr) values ('458-02-8317', 'Laurélie', 'Pennino', 'Stockholm', 'Stockholm', '114 39', 'jpennino6@webmd.com', '2805267028');
+insert into person (personal_id, first_name, last_name, city, state, postal_code, email_address, phone_nr) values ('585-74-1531', 'Réservés', 'Ivanyushin', 'Älvsjö', 'Stockholm', '125 46', 'mivanyushin7@usgs.gov', '2185844802');
+
+-- student
+insert into student (student_id, nr_of_siblings, guardian_id) values ('88-163-9046', 2, '6808057192');
+insert into student (student_id, nr_of_siblings, guardian_id) values ('22-071-4173', 2, '0965273563');
+insert into student (student_id, nr_of_siblings, guardian_id) values ('58-819-7581', 3, '5990481888');
+insert into student (student_id, nr_of_siblings, guardian_id) values ('49-940-3586', 2, '2857828438');
+insert into student (student_id, nr_of_siblings, guardian_id) values ('25-048-2785', 1, '1413777619');
+insert into student (student_id, nr_of_siblings, guardian_id) values ('21-414-3279', 3, '9925094879');
+insert into student (student_id, nr_of_siblings, guardian_id) values ('34-948-6221', 2, '7637040267');
+insert into student (student_id, nr_of_siblings, guardian_id) values ('55-715-6794', 2, '8923681377');
+
